07f175ab8c2e47e344738bb519d07bd0a1059891
Alejandro Ramos <arhcoder@gmail.com>
2025-01-08 02:20:54 -0600

    Chord class added. Another Block of construction for the whole project
    Chord object example looks like this: Chord(name=C, ctype=maj7, inversion=0, octave=4, time=4, dot=False notes=[Note(note=C, octave=4, frequency=261.626 Hz, time=4, name=Quarter dot=False space=16, tuning=440 Hz), Note(note=E, octave=4, frequency=329.628 Hz, time=4, name=Quarter dot=False space=16, tuning=440 Hz), Note(note=G, octave=4, frequency=391.995 Hz, time=4, name=Quarter dot=False space=16, tuning=440 Hz), Note(note=B, octave=4, frequency=493.883 Hz, time=4, name=Quarter dot=False space=16, tuning=440 Hz)], notes_str=['C4', 'E4', 'G4', 'B4'])

---
+++ src/Blocks/Chord.py
@@ -0,0 +1,265 @@
+from Note import Note
+patterns = ["", "m", "7", "maj7", "m7", "mmaj7", "aug", "dim", "sus2", "sus4"]
+
+class Chord:
+
+    def __init__(self, name: str, ctype: str, inversion: int, octave: int, time: int, dot: bool = False, tuning: int = 440):
+        '''
+            Paremeters:
+            - name [str]: Indicates the name of the chord based on the root note.
+                It could be some of this values:
+                ["C", "D", "E", "F", "G", "A", "B"]
+                Or some of this values:
+                ["C#", "D#", "F#", "G#", "A#"] or ["Db", "Eb", "Gb", "Ab", "Bb"]
+                "X" value represents a silent chords.
+            - ctype [str]: The type of the chord. It could be:
+                - "": Empty indicates Major chord.
+                - "m": Minor chord.
+                - "7": Major chord with minor 7th.
+                - "maj7": Major chord with major 7th.
+                - "m7": Minor with minor 7th.
+                - "mmaj7": Minor with major 7t.
+                - "aug" Augmented chord.
+                - "dim": Diminished chord.
+                - "sus2": Suspended 2nd chord.
+                - "sus4": Suspended 4th chord.
+            - inversion [int] Indicates if the chord has an inversion (only between 0 and 3):
+                - If 0, it will be in fundamental state.
+                - If 1, 2 or 3, it will be applied the inversions.
+            - octave [int]: Octave of the chord root (before apply an inversion).
+            - time [int]: Base time value for the chord:
+                ----------------------
+                1   Whole
+                2   Half
+                4   Quarter
+                8   Eighth
+                16  Sixteenth
+                32  Thirty-second
+                64  Sixty-fourth
+                ----------------------
+                3   Half Triplet
+                6   Quarter Triplet
+                12   Eighth Triplet
+                24   Sixteenth Triplet
+                ----------------------
+        '''
+
+        #/ ATTRIBUTES:
+        #? Chord name:
+        naturals = ["C", "D", "E", "F", "G", "A", "B", "X"]
+        sharps = ["C#", "D#", "F#", "G#", "A#"]
+        flats = ["Db", "Eb", "Gb", "Ab", "Bb"]
+        name = name.capitalize()
+        if name in flats:
+            name = sharps[flats.index(name)]
+        if name not in naturals + sharps + flats:
+            raise ValueError(f"\"name\" must be one of {naturals + sharps + flats}, but given {name}")
+        self._name = name
+        
+        #? Chord type:
+        ctype = ctype.lower()
+        if ctype not in patterns:
+            raise ValueError(f"\"ctype\" must be one of {patterns}, but given {ctype}")
+        self._ctype = ctype
+
+        #? Chord inversion:
+        if inversion < 0 or inversion > 3 or not isinstance(inversion, int):
+            raise ValueError(f"\"inversion\" must be an integer between 0 and 3, but given {inversion}")
+        self._inversion = inversion
+
+        #? Root octave:
+        if octave < 0 or octave > 8 or not isinstance(octave, int):
+            raise ValueError(f"\"octave\" must be an integer between 0 and 8, but given {octave}")
+        self._octave = octave
+
+        #? Chord time:
+        if time not in [1, 2, 4, 8, 16, 32, 64, 3, 6, 12, 24]:
+            raise ValueError(f"\"time\" must be one of: [1, 2, 4, 8, 16, 32, 64] or for tripletes: [3, 6, 12, 24], but given {time}")
+        self._time = time
+
+        #? Existance of Dot in the note:
+        if not isinstance(dot, bool):
+            raise TypeError(f"\"dot\" has to be boolean (True or False), but given {dot}")
+        self._dot = dot
+
+        #? Chord tuning:
+        if tuning not in [440, 432]:
+            raise ValueError(f"\"tuning\" must be either 440 or 432, but given {tuning}")
+        self._tuning = tuning
+
+        #? Calculated:
+        self._notes = None
+        self._notes_str = None
+        self._calculate_notes()
+    
+
+    #/ METHODS:
+    def _calculate_notes(self):
+
+        # If silent chord, ignore the rest:
+        if self._name == "X":
+            self._ctype = None
+            self._inversion = None
+            self._octave = None
+            self._notes = [Note(self._time, "X", 0, self._dot, self._tuning)]
+            self._notes_str = []
+            return
+
+        #? Constants:
+        scale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+        patterns = {
+            "": [0, 4, 7],
+            "m": [0, 3, 7],
+            "7": [0, 4, 7, 10],
+            "maj7": [0, 4, 7, 11],
+            "m7": [0, 3, 7, 10],
+            "mmaj7": [0, 3, 7, 11],
+            "aug": [0, 4, 8],
+            "dim": [0, 3, 6],
+            "sus2": [0, 2, 7],
+            "sus4": [0, 5, 7]
+        }
+
+        # Gets the root and the intervals:
+        root_index = scale.index(self._name)
+        intervals = patterns[self._ctype]
+        notes = []
+        notes_obj = []
+        notes_str = []
+        current_octave = self._octave
+
+        # Get each note of the chord:
+        for _, interval in enumerate(intervals):
+            note_index = (root_index + interval) % len(scale)
+            note_name = scale[note_index]
+
+            # Adjust the octave:
+            if root_index + interval >= len(scale):
+                current_octave += 1
+
+            notes.append({"note": note_name, "octave": current_octave})
+
+        # Apply inversions:
+        for _ in range(self._inversion):
+            inverted_note = notes.pop(0)
+            inverted_note["octave"] += 1
+            notes.append(inverted_note)
+        
+
+        # Creates two lists for notes:
+        for note in notes:
+            # Append the note using class Note:
+            notes_obj.append(Note(self._time, note["note"], note["octave"], self._dot, self._tuning))
+
+            # Generates also a easy lecture of the chord notes:
+            notes_str.append(f'{note["note"]}{note["octave"]}')
+
+        self._notes = notes_obj
+        self._notes_str = notes_str
+    
+
+    #/ SETTERS:
+    #? NAME:
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        naturals = ["C", "D", "E", "F", "G", "A", "B", "X"]
+        sharps = ["C#", "D#", "F#", "G#", "A#"]
+        flats = ["Db", "Eb", "Gb", "Ab", "Bb"]
+        value = value.capitalize()
+        if value in flats:
+            value = sharps[flats.index(value)]
+        if value not in naturals + sharps + flats:
+            raise ValueError(f"\"name\" must be one of {naturals + sharps + flats}, but given {value}")
+        if value != self._name:
+            self._name = value
+            self._calculate_notes()
+    
+    #? CTYPE:
+    @property
+    def ctype(self):
+        return self._ctype
+    
+    @ctype.setter
+    def ctype(self, value):
+        value = value.lower()
+        if value not in patterns:
+            raise ValueError(f"\"value\" must be one of {patterns}, but given {value}")
+        if value != self._ctype:
+            self._ctype = value
+            self._calculate_notes()
+    
+    #? CHORD INVERSION:
+    @property
+    def inversion(self):
+        return self._inversion
+    
+    @inversion.setter
+    def inversion(self, value):
+        if value < 0 or value > 3 or not isinstance(value, int):
+            raise ValueError(f"\"inversion\" must be an integer between 0 and 3, but given {value}")
+        if value != self._inversion:
+            self._inversion = value
+            self._calculate_notes()
+    
+    #? OCTAVE:
+    @property
+    def octave(self):
+        return self._octave
+
+    @octave.setter
+    def octave(self, value):
+        if value < 0 or value > 8 or not isinstance(value, int):
+            raise ValueError(f"\"octave\" must be an integer between 0 and 8, but given {value}")
+        if value != self._octave:
+            self._octave = value
+            self._calculate_notes()
+
+    #? TIME:
+    @property
+    def time(self):
+        return self._time
+    
+    @time.setter
+    def time(self, value):
+        if value not in [1, 2, 4, 8, 16, 32, 64, 3, 6, 12, 24]:
+            raise ValueError(f"\"time\" must be one of: [1, 2, 4, 8, 16, 32, 64] or for tripletes: [3, 6, 12, 24], but given {value}")
+        if value != self._time:
+            self._time = value
+            self._calculate_notes()
+    
+    #? DOT:
+    @property
+    def dot(self):
+        return self._dot
+
+    @dot.setter
+    def dot(self, value):
+        if not isinstance(value, bool):
+            raise TypeError(f"\"dot\" has to be boolean (True or False), but given {value}")
+        if value != self._dot:
+            self._dot = value
+            self._calculate_notes()
+    
+    #? TUNING:
+    @property
+    def tuning(self):
+        return self._tuning
+
+    @tuning.setter
+    def tuning(self, value):
+        if value not in [440, 432]:
+            raise ValueError(f"\"tuning\" must be either 440 or 432, but given {value}")
+        if value != self._tuning:
+            self._tuning = value
+            self._calculate_notes()
+    
+
+    def __repr__(self):
+        return (
+            f"Chord(name={self._name}, ctype={self._ctype}, inversion={self._inversion}, "
+            f"octave={self._octave}, time={self._time}, dot={self._dot} notes={self._notes}, notes_str={self._notes_str})"
+        )
--- src/Blocks/test.py
+++ src/Blocks/test.py
@@ -1,4 +1,6 @@
 from Note import Note
+from Chord import Chord
+
 
 #/ TESTING NOTES:
 note1 = Note(4, "A", 4)
@@ -34,3 +36,59 @@ print(note6)
 
 # note9 = Note(6, "C", 4.5)
 # print(note9)
+
+
+#/ TESTING CHORDS:
+chord1 = Chord(name="C", ctype="", inversion=0, octave=4, time=4)
+chord2 = Chord(name="C", ctype="", inversion=1, octave=4, time=4)
+chord3 = Chord(name="C", ctype="", inversion=2, octave=4, time=4)
+chord4 = Chord(name="C", ctype="", inversion=3, octave=4, time=4)
+
+chord5 = Chord(name="C", ctype="maj7", inversion=0, octave=4, time=4)
+chord6 = Chord(name="C", ctype="maj7", inversion=1, octave=4, time=4)
+chord7 = Chord(name="C", ctype="maj7", inversion=2, octave=4, time=4)
+chord8 = Chord(name="C", ctype="maj7", inversion=3, octave=4, time=4)
+chord9 = Chord(name="C", ctype="maj7", inversion=0, octave=5, time=4)
+
+chord10 = Chord(name="F#", ctype="7", inversion=0, octave=2, time=4)
+chord11 = Chord(name="Bb", ctype="m7", inversion=0, octave=3, time=4)
+chord12 = Chord(name="B", ctype="sus2", inversion=0, octave=3, time=4)
+chord13 = Chord(name="Ab", ctype="dim", inversion=3, octave=1, time=4)
+
+chord14 = Chord(name="X", ctype="", inversion=0, octave=1, time=4)
+
+print(chord1, end="\n\n")
+print(chord2, end="\n\n")
+print(chord3, end="\n\n")
+print(chord4, end="\n\n")
+print(chord5, end="\n\n")
+print(chord6, end="\n\n")
+print(chord7, end="\n\n")
+print(chord8, end="\n\n")
+print(chord9, end="\n\n")
+print(chord10, end="\n\n")
+print(chord11, end="\n\n")
+print(chord12, end="\n\n")
+print(chord13, end="\n\n")
+print(chord14, end="\n\n")
+
+print("\nCHORDS CHANGES: -------------------------------------")
+chord16 = Chord(name="C", ctype="", inversion=0, octave=4, time=4)
+print(chord16, end="\n\n")
+chord16.name = "F"
+print(chord16, end="\n\n")
+chord16.ctype = "m"
+print(chord16, end="\n\n")
+chord16.inversion = 2
+print(chord16, end="\n\n")
+chord16.octave = 2
+print(chord16, end="\n\n")
+chord16.time = 1
+print(chord16, end="\n\n")
+chord16.dot = True
+print(chord16, end="\n\n")
+chord16.tuning = 432
+print(chord16, end="\n\n")
+chord16.name = "X"
+chord16.tuning = 440
+print(chord16, end="\n\n")
