e388267c16159fb708de113f6cc2171d4f340f28
Alejandro Ramos <arhcoder@gmail.com>
2025-01-08 18:04:06 -0600

    Not important change in utils directory

---
+++ Utils/Rythm/sentence_to_rythm_melody.py
@@ -0,0 +1,10 @@
+
+# Sentences in spanish:
+sentence1 = "Voz de la guitarra mía"
+sentence2 = "Al despertar la mañana"
+sentence3 = "Viene a cantar su alegría"
+sentence4 = "A mi tierra mexicana"
+
+# How many bars of which signature has to take one sentence:
+signtaure = (4, 4)
+bars = 4
