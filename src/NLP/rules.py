40c392373a6fa9788e69536a258b20717bec56de
Alejandro Ramos <arhcoder@gmail.com>
2025-01-16 18:06:35 -0600

    Grammar Components v.1.0.1
    Maybe a better aproximation on the sentences logic fixed on the grammar production rules

--- src/NLP/rules.py
+++ src/NLP/rules.py
@@ -7,19 +7,19 @@ simple_declarative = ("Simple Declarative Sentence",
 S -> Sujeto Predicado
 
 # Reglas para Sujeto:
-Sujeto -> Det Sus Adj | PronPer | PronDem | Sus
+Sujeto -> Det Sus | Det Sus Adj | PronPer | PronDem Sus | PronDem Sus Adj
 
 # Reglas para Predicado:
 Predicado -> Verbo Objeto Circunstancial | Verbo Objeto | Verbo Circunstancial | Verbo
 
 # Reglas para Verbo:
-Verbo -> VTran | VIntra | VCop
+Verbo -> VTran | VIntra | VConj | VCop Adj
 
 # Reglas para Objeto:
-Objeto -> Det Sus Adj | PronRef | PronPrep | Sus
+Objeto -> Det Sus | Det Sus Adj | PronRef | PronPrep | Sus
 
 # Reglas para Circunstancial:
-Circunstancial -> Prep Sus Adj | Prep Sus | Adv
+Circunstancial -> Prep Sus | Prep Sus Adj | Adv
 
 # Terminales (categorías gramaticales):
 Det -> 'Det'
@@ -30,77 +30,66 @@ PronDem -> 'PronDem'
 VTran -> 'VTran'
 VIntra -> 'VIntra'
 VCop -> 'VCop'
+VConj -> 'VConj'
 PronRef -> 'PronRef'
 PronPrep -> 'PronPrep'
 Prep -> 'Prep'
 Adv -> 'Adv'
-""")
+"""
+)
 
 
 #? [02] Simple Interrogative Sentences:
 simple_interrogative = ("Simple Interrogative Sentence",
 """
-# Oración Interrogativa Completa:
-S -> PartInterr Verbo Sujeto Objeto Circunstancial
-S -> PartInterr Verbo Sujeto Objeto
+# Oración Interrogativa Simplificada:
 S -> PartInterr Verbo Sujeto Circunstancial
 S -> PartInterr Verbo Sujeto
-S -> PartInterr Sujeto Verbo Objeto Circunstancial
-S -> PartInterr Sujeto Verbo Objeto
-S -> PartInterr Sujeto Verbo Circunstancial
-S -> Verbo Sujeto Objeto Circunstancial PartInterr
+S -> PartInterr Verbo Objeto
+S -> PartInterr Verbo
 S -> Verbo Sujeto Objeto PartInterr
 S -> Verbo Sujeto Circunstancial PartInterr
-S -> PartInterr Verbo
+S -> PartInterr Sujeto Verbo
 
 # Partículas Interrogativas:
-PartInterr -> 'PronIntExc'
-PartInterr -> 'Adv'
+PartInterr -> PronIntExc
+PartInterr -> AdvInt
 
 # Sujeto:
-Sujeto -> Det Sus Adj
-Sujeto -> 'PronPer'
-Sujeto -> 'PronDem'
-Sujeto -> 'Sus'
+Sujeto -> PronPer
+Sujeto -> PronDem
+Sujeto -> Sus
 
 # Verbo:
-Verbo -> 'VTran'
-Verbo -> 'VIntra'
-Verbo -> 'VCop'
-Verbo -> 'VConj'
+Verbo -> VTran
+Verbo -> VIntra
+Verbo -> VCop Adj
+Verbo -> VConj
+Verbo -> VCop
 
 # Objeto:
-Objeto -> Det Sus Adj
-Objeto -> 'PronRef'
-Objeto -> 'PronPrep'
-Objeto -> 'Sus'
+Objeto -> Det Sus
+Objeto -> PronRef
+Objeto -> Sus
 
 # Circunstancial:
-Circunstancial -> Prep Sus Adj
 Circunstancial -> Prep Sus
-Circunstancial -> 'Adv'
+Circunstancial -> Adv
 
-# Determinantes:
+# Terminales (categorías gramaticales):
 Det -> 'Det'
-
-# Sustantivos:
 Sus -> 'Sus'
-
-# Adjetivos:
-Adj -> 'Adj'
-
-# Preposiciones:
-Prep -> 'Prep'
-
-# Adverbios:
-Adv -> 'Adv'
-
-# Pronombres:
-PronIntExc -> 'PronIntExc'
 PronPer -> 'PronPer'
-PronRef -> 'PronRef'
-PronPrep -> 'PronPrep'
 PronDem -> 'PronDem'
+VTran -> 'VTran'
+VIntra -> 'VIntra'
+VCop -> 'VCop'
+VConj -> 'VConj'
+PronRef -> 'PronRef'
+Prep -> 'Prep'
+Adv -> 'Adv'
+AdvInt -> 'PronIntExc'
+PronIntExc -> 'PronIntExc'
 """
 )
 
@@ -109,30 +98,75 @@ PronDem -> 'PronDem'
 exclamative_sentence = ("Exclamative Sentence",
 """
 # Oración Exclamativa Completa:
+S -> Int PartExcl Verbo Sujeto Objeto Circunstancial
+S -> Int PartExcl Verbo Sujeto Circunstancial
+S -> Int PartExcl Verbo Sujeto Objeto
+S -> Int PartExcl Verbo Sujeto
+S -> Int PartExcl Sujeto Verbo Objeto Circunstancial
+S -> Int PartExcl Sujeto Verbo Circunstancial
+S -> Int PartExcl Sujeto Verbo Objeto
+S -> Int PartExcl Sujeto Verbo
 S -> PartExcl Verbo Sujeto Objeto Circunstancial
-S -> PartExcl Sujeto Verbo Objeto Circunstancial
+S -> PartExcl Verbo Sujeto Circunstancial
+S -> PartExcl Sujeto Verbo Objeto
 S -> PartExcl Sujeto Verbo Circunstancial
-S -> PartExcl Verbo Sujeto
-S -> Verbo Sujeto Objeto Circunstancial PartExcl
-S -> Sujeto Verbo PartExcl
+S -> PartExcl Verbo Objeto
 S -> PartExcl Objeto
 S -> PartExcl Circunstancial
 
 # Partículas Exclamativas:
-PartExcl -> 'PronIntExc' | 'Adv' | 'Adj'
+PartExcl -> PronIntExc
+PartExcl -> AdvInt
+PartExcl -> Adj
 
 # Sujeto:
-Sujeto -> 'Det Sus Adj' | 'PronPer' | 'PronDem' | 'Sus'
+Sujeto -> Det Sus Adj
+Sujeto -> Det Sus
+Sujeto -> PronPer
+Sujeto -> PronDem Sus
+Sujeto -> Sus
 
 # Verbo:
-Verbo -> 'VTran' | 'VIntra' | 'VCop'
+Verbo -> VTran
+Verbo -> VIntra
+Verbo -> VCop Adj
+Verbo -> VCop
+Verbo -> VConj
 
 # Objeto:
-Objeto -> 'Det Sus Adj' | 'PronRef' | 'PronPrep' | 'Sus'
+Objeto -> Det Sus Adj
+Objeto -> Det Sus
+Objeto -> PronRef
+Objeto -> PronPrep
+Objeto -> Sus
 
 # Circunstancial:
-Circunstancial -> 'Prep Sus Adj' | 'Prep Sus' | 'Adv'
-""")
+Circunstancial -> Prep Sus Adj
+Circunstancial -> Prep Sus
+Circunstancial -> Adv
+
+# Interjecciones:
+Int -> 'Int'
+
+# Terminales (categorías gramaticales):
+Det -> 'Det'
+Sus -> 'Sus'
+Adj -> 'Adj'
+PronPer -> 'PronPer'
+PronDem -> 'PronDem'
+VTran -> 'VTran'
+VIntra -> 'VIntra'
+VCop -> 'VCop'
+VConj -> 'VConj'
+PronRef -> 'PronRef'
+PronPrep -> 'PronPrep'
+Prep -> 'Prep'
+Adv -> 'Adv'
+AdvInt -> 'PronIntExc'
+PronIntExc -> 'PronIntExc'
+Int -> 'Int'
+"""
+)
 
 
 #? [04] Imperative Sentences:
@@ -142,30 +176,34 @@ imperative_sentence = ("Imperative Sentence",
 S -> VerboImp SujetoImplícito Objeto Circunstancial
 S -> VerboImp Objeto Circunstancial
 S -> VerboImp Circunstancial
-S -> SujetoExplícito VerboImp Objeto Circunstancial
 S -> VerboImp Objeto
+S -> SujetoExplícito VerboImp Objeto Circunstancial
 S -> VerboImp
+S -> SujetoExplícito VerboImp
+S -> VerboImp SujetoImplícito
 
-# Sujeto Implícito:
+# Sujeto Implícito (omisión natural en oraciones imperativas):
 SujetoImplícito -> ''
 
-# Sujeto Explícito:
-SujetoExplícito -> PronPer 'PronPer'
-SujetoExplícito -> Sus Det 'Sus' 'Det'
+# Sujeto Explícito (cuando se necesita énfasis o claridad):
+SujetoExplícito -> PronPer
+SujetoExplícito -> Sus Det
 
 # Verbo en Imperativo:
 VerboImp -> 'VImp'
 
-# Objeto:
-Objeto -> Det Sus Adj 'Det' 'Sus' 'Adj'
-Objeto -> PronRef 'PronRef'
-Objeto -> PronDem 'PronDem'
-Objeto -> PronPer 'PronPer'
+# Objeto (elementos directos que complementan el verbo):
+Objeto -> Det Sus
+Objeto -> Det Sus Adj
+Objeto -> PronRef
+Objeto -> PronDem
+Objeto -> PronPer
 
-# Circunstancial:
-Circunstancial -> Prep Sus Adj 'Prep' 'Sus' 'Adj'
-Circunstancial -> Prep Sus 'Prep' 'Sus'
-Circunstancial -> Adv 'Adv'
-""")
+# Circunstancial (complementos opcionales de lugar, tiempo o modo):
+Circunstancial -> Prep Sus Adj
+Circunstancial -> Prep Sus
+Circunstancial -> Adv
+"""
+)
 
 sentences_rules = [simple_declarative, simple_interrogative, exclamative_sentence, imperative_sentence]
